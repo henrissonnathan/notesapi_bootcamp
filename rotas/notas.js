8a1b2f2a13f5e653a3d7d23e53e8d04138e0a135
henrisson braga <henrisson2000@gmail.com>
2021-11-17 22:32:37 -0300

    fsd

--- rotas/notas.js
+++ rotas/notas.js
@@ -2,9 +2,12 @@ const { Router } = require("express");
 const express = require("express");
 const router = express.Router();
 
-router.get("/",function(req,res){
-    res.send("Get nota")
-
+router.get("/:id?",function(req,res){
+    if(req.params.id){
+    res.send("Nota ID "+ req.params.id)
+    }else{
+        res.send("todas as notas")
+    }
 });
 
 router.post("/",function(req,res){
